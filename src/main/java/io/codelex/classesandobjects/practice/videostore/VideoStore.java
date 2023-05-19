23cd64156d4638ab39420aa920f2054f2529f185
Mārtiņš Sliede <martinssliede@gmail.com>
2023-05-19 09:56:17 +0300

    Failed attempt on Videostore

---
+++ src/main/java/io/codelex/classesandobjects/practice/videostore/VideoStore.java
@@ -0,0 +1,12 @@
+package io.codelex.classesandobjects.practice.videostore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoStore {
+    private List<Video> inventory = new ArrayList<>();
+
+    public void addToInventory(String title, int rating) {
+
+    }
+}

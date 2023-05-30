58b04f01ebcebb7e703f58c41246b9c31b7b377f
Mārtiņš Sliede <martinssliede@gmail.com>
2023-05-30 18:30:16 +0300

    Latest commit

--- src/main/java/io/codelex/collections/practice/Histogram.java
+++ src/main/java/io/codelex/collections/practice/Histogram.java
@@ -1,5 +1,7 @@
 package io.codelex.collections.practice;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
@@ -14,6 +16,26 @@ public class Histogram {
     public static void main(String[] args) throws IOException, URISyntaxException {
         final String scores = fileContent();
         System.out.println(scores);
+
+        int[] ranges = new int[11];
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+        String line;
+        while ((line = reader.readLine()) != null) {
+            int score = Integer.parseInt(line);
+            int rangeIndex = score / 10;
+            ranges[rangeIndex]++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+            return;
+        }
+
+        for (int i = 0; i < ranges.length; i++) {
+            String range = String.format("%02d - %02d: ", i * 10, 1 * 10 + 9);
+            String asterisks = "*".repeat(ranges[i]);
+            System.out.println(range + " " + asterisks);
+        }
+        System.out.println("100 : " + "*".repeat(ranges[10]));
     }
 
     private static String fileContent() throws URISyntaxException, IOException {
--- src/main/java/io/codelex/collections/practice/WordCount.java
+++ src/main/java/io/codelex/collections/practice/WordCount.java
@@ -8,38 +8,37 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class WordCount {
     private static final Charset charset = Charset.defaultCharset();
     private static final String file = "/collections/lear.txt";
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-//        Kaut kas nav ar file linku??
-//        final Path path = Paths.get(Histogram.class.getResource(file).toURI());
-//        Files.readAllLines(path, charset);
-//
-//        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-//            int lineCount = 0;
-//            int wordCount = 0;
-//            int charCount = 0;
-//
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                lineCount++;
-//                charCount += line.length();
-//
-//                String[] words = line.split("\\s+");
-//                for (String word : words) {
-//                    if (!word.isEmpty() && Character.isLetterOrDigit(word.charAt(0))) {
-//                        wordCount++;
-//                    }
-//                }
-//            }
-//            System.out.println("Line count: " + lineCount);
-//            System.out.println("Word count: " + wordCount);
-//            System.out.println("Char count: " + charCount);
-//        } catch (IOException e) {
-//            System.out.println("Error reading the file " + e.getMessage());
-//        }
+
+        try {
+            final Path path = Paths.get(Histogram.class.getResource(file).toURI());
+            List<String> allFileLines = Files.readAllLines(path, charset);
+
+            int lineCount = allFileLines.size();
+            int wordCount = 0;
+            int charCount = 0;
+
+            for (String line : allFileLines) {
+                charCount += line.length();
+
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    if (!word.isEmpty() && Character.isLetterOrDigit(word.charAt(0))) {
+                        wordCount++;
+                    }
+                }
+            }
+            System.out.println("Line count: " + lineCount);
+            System.out.println("Word count: " + wordCount);
+            System.out.println("Char count: " + charCount);
+        } catch (IOException e) {
+            System.out.println("Error reading the file " + e.getMessage());
+        }
     }
 }
